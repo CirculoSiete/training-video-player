adce9d5f47e474fb74a94249945558a553937f9e
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-11-04 14:26:19 -0600

    init

---
+++ src/main/java/training/video/player/Application.java
@@ -0,0 +1,18 @@
+package training.video.player;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "training-video-player",
+            version = "0.0"
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
